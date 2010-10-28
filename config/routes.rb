9d69a23cabeb6ac3434b15225a019014777ccffb
GordonDiggs <gordon@gordondiggs.com>
2010-10-27 21:50:50 -0400

    Revert "add authlogic to app"
    This reverts commit c1c071281378e11fb2ceddbed2319d94ebd714c2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :user_session
-  
   # root behavior
   map.root :controller => 'home', :action => :index
-  
+
   # default routes
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101028011107) do
+ActiveRecord::Schema.define(:version => 20101028004258) do
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -17,23 +17,10 @@ ActiveRecord::Schema.define(:version => 20101028011107) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "login",                             :null => false
-    t.string   "password_salt",                     :null => false
-    t.string   "persistence_token",                 :null => false
-    t.string   "perishable_token",                  :null => false
-    t.integer  "login_count",        :default => 0, :null => false
-    t.integer  "failed_login_count", :default => 0, :null => false
-    t.datetime "last_request_at"
-    t.datetime "current_login_at"
-    t.datetime "last_login_at"
   end
 
   add_index "users", ["email_address"], :name => "index_users_on_email_address"
   add_index "users", ["first_name"], :name => "index_users_on_first_name"
-  add_index "users", ["last_login_at"], :name => "index_users_on_last_login_at"
   add_index "users", ["last_name"], :name => "index_users_on_last_name"
-  add_index "users", ["last_request_at"], :name => "index_users_on_last_request_at"
-  add_index "users", ["login"], :name => "index_users_on_login"
-  add_index "users", ["login_count"], :name => "index_users_on_login_count"
 
 end
